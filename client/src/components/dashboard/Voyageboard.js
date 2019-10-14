a108a2e86525d3905bf4c6f83b5cd0ea8d28f65b
cindybox <cindybox.33@gmail.com>
2019-10-13 20:33:28 -0500

    add placeholder components for client

---
+++ client/src/components/dashboard/Voyageboard.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const VoyageBoard = props => {
+    return (
+        <div>
+            Voyage Board
+        </div>
+    )
+}
+
+VoyageBoard.propTypes = {
+
+}
+
+export default VoyageBoard
